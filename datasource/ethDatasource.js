31c00e30e077cd9a7a43b6ae73e2903bedf630cf
monika_mei <monika.meiliana@gmail.com>
2023-05-24 16:33:59 +0700

    first commit

---
+++ datasource/ethDatasource.js
@@ -0,0 +1,44 @@
+const { RESTDataSource } = require("apollo-datasource-rest");
+
+//Vitalik's Ethereum Address
+const eth_address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
+
+//Etherscan Data Source Class
+class EtherDataSource extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = "https://api.etherscan.io/api";
+  }
+
+  async etherBalanceByAddress() {
+    return this
+      .get
+      //Insert API Endpoint - For Get Ether Balance for a Single Address
+      (
+        `${this.baseURL}?module=account&action=balance&address=${eth_address}&tag=latest&apikey=${process.env.ETHERSCAN_API}`,
+        {},
+        {
+          headers: {
+          accept: "application/json"
+          },
+        }
+      );
+  }
+
+  async totalSupplyOfEther() {
+    return this
+      .get
+      //Insert API Endpoint - For Get Total Supply of Ether
+      (
+        `${this.baseURL}?module=stats&action=ethsupply&apikey=${process.env.ETHERSCAN_API}`,
+        {},
+        {
+          headers: {
+          accept: "application/json"
+          },
+        }
+      );
+  }
+}
+
+module.exports = EtherDataSource;
